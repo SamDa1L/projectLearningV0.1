1599fb0fd5a50f7192f34e2e2b32bb32b636b4da
WSX <wsx283848@gmail.com>
2025-11-02 11:51:45 +0800

    人物横向移动上传

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//场景视差效果参考视频：https://www.youtube.com/watch?v=tMXgLBwtsvI
+
+public class ParallaxEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
